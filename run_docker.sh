e214d72f106bcd8b7ceb2f803776e5a6b5f9bf2c
Ibrahim Aljumah Toure <66955118+ialjumah@users.noreply.github.com>
2021-11-21 01:26:29 +0000

    Update run_docker.sh
    updating and checking script files

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+# This tags and uploads an image to Docker Hub
 
 # Step 1:
-# Build image and add a descriptive tag
+# This is your Docker ID/path
+dockerpath=bashox/col
 
-# Step 2: 
-# List docker images
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run container --image=$dockerpath
 
-# Step 3: 
-# Run flask app
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+yum install socat -y
+kubectl port-forward container 8000:80
